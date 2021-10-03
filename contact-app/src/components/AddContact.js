6d3ba2c2d6f794a32f259aee64bb9aa65d3a422b
nikhilnitrr <nikhilchandrakar3@gmail.com>
2021-10-03 20:24:29 +0530

    Initial Project Setup

---
+++ contact-app/src/components/AddContact.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function AddContact() {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="Enter your name" />
+        <input type="email" placeholder="Enter your email" />
+      </form>
+    </div>
+  );
+}
+
+export default AddContact;
